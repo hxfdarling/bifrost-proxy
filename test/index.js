739532be7ca2310026ab89135c488482c4498bd3
liuhua.jia <liuhua.jia@bytedance.com>
2022-02-15 15:40:17 +0800

    test: 添加测试用例

--- test/index.js
+++ test/index.js
@@ -1,9 +1,28 @@
 process.env.BIFROST = true;
 require('../lib/index');
 const fetch = require('node-fetch');
+const WebSocket = require('ws');
 
 fetch('https://act.snssdk.com/test/', {
   headers: {
     // host: '127.0.0.1:8080',
   },
-}).then(console.log);
+}).then((r) => console.log(`status:${r.status}`));
+
+const ws = new WebSocket('ws://121.40.165.18:8800');
+ws.onopen = function () {
+  console.log('连接服务器成功');
+  // 向服务器发送消息
+  ws.send('what`s your name?');
+};
+ws.onclose = function () {
+  console.log('服务器关闭');
+};
+ws.onerror = function () {
+  console.log('连接出错');
+};
+// 接收服务器的消息
+ws.onmessage = function (e) {
+  const message = `message:${e.data}`;
+  console.log(message);
+};
